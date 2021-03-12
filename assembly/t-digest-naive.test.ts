0546f5a4c71daa7aac08c2d2b98026b4875ba336
bcolloran <brendan@colloran.net>
2021-03-12 12:35:14 -0800

    naive t digest core working

--- assembly/t-digest-naive.test.ts
+++ assembly/t-digest-naive.test.ts
@@ -3,6 +3,7 @@ import {
   centroidsFromPoints,
   centroidSortFn,
   mergeData,
+  estimateQuantile,
 } from "./t-digest-naive";
 
 describe("centroids", () => {
@@ -45,37 +46,45 @@ describe("centroidsFromPoints", () => {
   });
 });
 
-function logisticInvCdf(y: f64, mu: f64 = 0.0, s: f64 = 1.0): f64 {
-  return mu - s * Math.log(1 / y - 1);
-}
-
 describe("mergeData", () => {
   it("total weight must be correct", () => {
     const N = 10000;
     const X = new Array<f64>(N)
       .fill(0)
       .map<f64>((_, i) => ((1.0 * i) / N) ** 2);
-    // const C = centroidsFromPoints(X);
     const C = mergeData(centroidsFromPoints(X));
     const weight = C.reduce((x, c) => x + c.count, 0.0);
-
-    // log(C);
     expect(weight).toBe(N);
   });
+});
 
+function logisticInvCdf(y: f64, mu: f64 = 0.0, s: f64 = 1.0): f64 {
+  return mu - s * Math.log(1 / y - 1);
+}
+
+describe("estimateQuantile", () => {
   it("correct approximate quantiles for logistic(0,1) distribution", () => {
-    const N = 10000;
+    const N = 30000;
     const X = new Array<f64>(N)
       .fill(0)
-      .map<f64>(() => Math.random())
+      .map<f64>((_, i) => (1.0 * i + 1) / (N + 1))
       .map<f64>((x) => logisticInvCdf(x));
     // const C = centroidsFromPoints(X);
-    log("length X " + X.length.toString());
-    const C = mergeData(centroidsFromPoints(X), [], 50);
-    const weight = C.reduce((x, c) => x + c.count, 0.0);
+    // log("length X " + X.length.toString());
+    const C = mergeData(centroidsFromPoints(X), [], 100);
+    // const weight = C.reduce((x, c) => x + c.count, 0.0);
+    // log(C);
+    const x = estimateQuantile(C, 0.5);
 
-    log("length " + C.length.toString());
-    log(C);
-    expect(weight).toBe(N);
+    log("x est .05: " + estimateQuantile(C, 0.05).toString());
+    log("x est .5: " + estimateQuantile(C, 0.5).toString());
+    log("x est .99: " + estimateQuantile(C, 0.99).toString());
+
+    log("logisticInvCdf  " + logisticInvCdf(0.05).toString());
+    log("logisticInvCdf  " + logisticInvCdf(0.5).toString());
+    log("logisticInvCdf  " + logisticInvCdf(0.99).toString());
+    expect(estimateQuantile(C, 0.5)).toBeCloseTo(logisticInvCdf(0.5));
+    expect(estimateQuantile(C, 0.05)).toBeCloseTo(logisticInvCdf(0.05));
+    expect(estimateQuantile(C, 0.99)).toBeCloseTo(logisticInvCdf(0.99));
   });
 });
--- assembly/t-digest-naive.ts
+++ assembly/t-digest-naive.ts
@@ -45,12 +45,12 @@ export function mergeData(
   let X = centroids.concat(newPoints);
   X = X.sort(centroidSortFn);
   const S = X.reduce((s, centroid) => s + centroid.count, 0.0);
-  log<f64>(S);
+  // log<f64>(S);
   let Cprime: Centroid[] = [];
   let q_0 = 0.0;
   let q_lim = KInvOfKFn(q_0, delta);
   // log<f64>(q);
-  log<f64>(q_lim);
+  // log<f64>(q_lim);
   let sigma = X[0];
   for (let i = 1, N = X.length; i < N; i++) {
     let q = q_0 + (sigma.count + X[i].count) / S;
@@ -69,10 +69,62 @@ export function mergeData(
   return Cprime;
 }
 
-/* 
- * @param tDigest is a sorted array of centroids
+export function interpolate(y: f64, y1: f64, y2: f64, x1: f64, x2: f64): f64 {
+  return x1 + ((x2 - x1) / (y2 - y1)) * (y - y1);
+}
 
+/**
+ * Returns a target quantile given a sorted t-digest. Note that we omit the
+ * additional handling of unit weight intervals, instead using the simplifying
+ * assumption that interval between *any* two consecutive centroid has weight
+ * equal to half the total weight of those centroids (in otherwords, each
+ * centroid has half of it's weight on each side of its mean). The only
+ * exceptions are the first and last "centroids", which contribute all of their
+ * weight to the following/preceding interval.
+ *
+ * @param X - A sorted array of centroids (i.e., a t-digest)
+ * @param q - The target quantile; a float in the interval [0,1]
  */
-export function estimateQuantile(tDigest: Centroid[], q: f64): f64 {
-  return;
+export function estimateQuantile(X: Centroid[], q: f64): f64 {
+  if (q == 0) return X[0].mean;
+
+  const N = X.length;
+  if (q == 1) return X[N - 1].mean;
+
+  const S = X.reduce((s, centroid) => s + centroid.count, 0.0);
+  // log("S inner:" + S.toString());
+
+  const targetWeight = S * q;
+  let weightIntervalLeft = 0.0;
+  let weightIntervalRight = X[0].count + 0.5 * X[1].count;
+
+  for (let i = 1; i < N - 1; i++) {
+    // weightIntervalRight += ;
+    if (targetWeight < weightIntervalRight) {
+      // log("targetWeight,   " + targetWeight.toString());
+      // log("weightIntervalLeft,   " + weightIntervalLeft.toString());
+      // log("weightIntervalRight,   " + weightIntervalRight.toString());
+      // log("X[i - 1].mean,   " + X[i - 1].mean.toString());
+      // log("X[i].mean   " + X[i].mean.toString());
+      return interpolate(
+        targetWeight,
+        weightIntervalLeft,
+        weightIntervalRight,
+        X[i - 1].mean,
+        X[i].mean
+      );
+    } else {
+      weightIntervalLeft = weightIntervalRight;
+      weightIntervalRight += 0.5 * (X[i].count + X[i + 1].count);
+    }
+  }
+
+  // q is in the last interval between N-2 and N-1
+  return interpolate(
+    targetWeight,
+    weightIntervalLeft,
+    S,
+    X[N - 2].mean,
+    X[N - 1].mean
+  );
 }
